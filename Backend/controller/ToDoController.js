e8b501865e43f4243fbf155dceeb45a0814961ef
Priya <pv858004@gmail.com>
2025-08-01 18:51:31 +0530

    Initial commit

---
+++ Backend/controller/ToDoController.js
@@ -0,0 +1,42 @@
+const ToDoModel = require('../Models/ToDoModels');
+module.exports.getToDos = async (req, res)=> {
+        const toDos = await ToDoModel.find();
+        res.send(toDos);
+}
+module.exports.saveToDo = async (req, res) => {
+    const {toDo} = req.body;
+    ToDoModel.create({toDo})
+    .then(data=>{
+        console.log("Saved Successfully");
+        res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ error: err, msg: "Something went wrong!" });
+    });
+}
+module.exports.updateToDo = (req, res) => {
+  const { id } = req.params;
+  const { toDo } = req.body;
+
+  ToDoModel.findByIdAndUpdate(id, { toDo })
+    .then(() => {
+      res.send("Updated Successfully....");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ error: err, msg: "Something went wrong!" });
+    });
+};
+module.exports.deleteToDo = (req, res) => {
+  const { id } = req.params;
+
+  ToDoModel.findByIdAndDelete(id)
+    .then(() => {
+      res.send("Deleted Successfully....");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ error: err, msg: "Something went wrong!" });
+    });
+};
---
+++ Backend/models/ToDoModels.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const ToDoSchema = new mongoose.Schema({
+    toDo: {
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model('ToDo', ToDoSchema)
